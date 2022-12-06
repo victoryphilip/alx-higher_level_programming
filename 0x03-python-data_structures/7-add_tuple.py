80b4db9684d0a0cfba92feef1be71f8879d38752
victoryphilip <victorymartin22@gmail.com>
2022-12-06 17:52:24 +0100

    all task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -14,4 +14,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = tuple_b[0], 0
 
     added_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    return 
+    return added_tuple 
