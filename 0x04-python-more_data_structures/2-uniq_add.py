f8df163bc2a952e747b7a795ab5902729135ad89
victoryphilip <victorymartin22@gmail.com>
2022-12-07 20:29:24 +0100

    all task

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    my_list_as_unique_set = set(my_list)
 
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return (my_list)
-    else:
-        my_list[idx] = element
-        return (my_list)
+    result = 0
+
+    for num in my_list_as_unique_set:
+        result += num
+
+    return result
