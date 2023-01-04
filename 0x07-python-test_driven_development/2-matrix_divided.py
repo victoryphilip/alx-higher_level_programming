deda934a87ceeba88c272ffb9f29b19b56891dad
victoryphilip <victorymartin22@gmail.com>
2023-01-04 06:50:10 +0100

    correc tion

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
 """
+
 This module is composed by a function that divides the numbers of a matrix
+
 """
 
 
 def matrix_divided(matrix, div):
     """ Function that divides the integer/float numbers of a matrix
+
     Args:
         matrix: list of a lists of integers/floats
         div: number which divides the matrix
+
     Returns:
         A new matrix with the result of the division
+
     Raises:
         TypeError: If the elements of the matrix aren't lists
                    If the elemetns of the lists aren't integers/floats
                    If div is not an integer/float number
                    If the lists of the matrix don't have the same size
+
         ZeroDivisionError: If div is zero
+
+
     """
 
     if not type(div) in (int, float):
