9da84d75140db58055340364cfc7c81436392618
victoryphilip <victorymartin22@gmail.com>
2023-03-16 07:00:33 +0100

    push 13-model_state_delete_a.py  file

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This script deletes all State objects
+with a name containing the letter `a`
+from the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
